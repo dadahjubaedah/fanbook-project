bdd47cf94237cc27d7dd8e7e489f8b21c3dd7f65
Dadah Jubaedah <dadahjubaedah@gmail.com>
2023-03-28 15:46:41 +0700

    First Commit

--- start.sh
+++ start.sh
@@ -12,7 +12,7 @@ if [ ! -f $VIRTUALENV/bin/pip ]; then
   curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV
 fi
 
-$VIRTUALENV/bin/pip install -r requirement.txt
+$VIRTUALENV/bin/pip install -r requirements.txt
 
 $VIRTUALENV/bin/python3 app.py
 Footer
